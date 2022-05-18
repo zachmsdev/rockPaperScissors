227799766d5cfba137a8230b8e70c7cbdc55257b
Zach <zachsneeringerdev@gmail.com>
2022-05-18 14:34:06 -0400

    Rock Paper Scissors - in Console

--- rPsApp.js
+++ rPsApp.js
@@ -1,2 +1,54 @@
 
-// Rock Paper Scissors App!
+// Rock Paper Scissors
+
+const computerPlay = () => {
+    const choices = [
+        'rock',
+        'paper',
+        'scissors'  
+    ];
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    console.warn('Computer chose: ' + randomChoice);
+    return randomChoice;
+}
+
+const playRound = (playerSelection, computerSelection) => {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        console.log('You win! Rock beats scissors!');
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        console.log('You lose.. Computer picked paper!');
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        console.log('You lose! Paper can\'t beat scissors!');
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        console.log('You win. Paper beats rock.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        console.log('You lose.. Computer picked Rock!');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        console.log('You win! Scissors beats paper! :D');
+    } else if (playerSelection === computerSelection) {         // tie game
+        console.log('Tie game!!');
+    } else {
+        console.log('Hmm, not working.. type in Rock / Paper / Scissors!');
+    }
+}
+
+const game = () => {
+    for (let z = 0; z < 5; z++) {
+        const playerSelection = prompt('Rock, Paper, or Scissors?');
+        const computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+        if (z >= 4 ) {
+            console.error('Game Over!');
+        } 
+    }
+}
+
+game();
+
+
+
+
+
+
+
